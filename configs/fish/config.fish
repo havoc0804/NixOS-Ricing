fbd175b45819502b1487b5e5e37b901047700cc4
havoc <224394689+havoc0804@users.noreply.github.com>
2026-06-22 22:24:51 +1000

    got fish aliases and starship prompt setup

--- configs/fish/config.fish
+++ configs/fish/config.fish
@@ -1,2 +1,24 @@
 # removing fish greeting
 set fish_greeting
+
+# Aliases
+
+# LS
+alias ls='eza -lah --classify --icons --group-directories-first' # pretty useful defulat command
+
+#File operations
+alias mv='mv -i' # confirms before doing
+alias rm='rm -i' # comifrms before deleting
+alias cp='cp -i' # confirms before copying
+
+#Git
+alias ga='git add .'
+alias gc='git commit -m'
+alias gp='git push'
+
+#NixOS Specifc
+alias rbs='sudo nixos-rebuild switch'
+
+#Misc
+alias s='sudo'
+alias n='nvim'
--- fish/config.fish
+++
@@ -1,7 +0,0 @@
-#removing the fish greeting
-set fish_greeting
-
-
-
-#using starship
-starship init fish | source
